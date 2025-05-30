156074aff203213e63158e457e28dee73865719c
Pyromagne <growganoth826@gmail.com>
2025-05-30 09:12:33 +0800

    fix build error

--- resources/js/Pages/Record.jsx
+++ resources/js/Pages/Record.jsx
@@ -1,5 +1,4 @@
 import useRole from "@/hooks/useRole";
-import { rnd_pdf_gen } from "@/functions/pdf_generators/rnd_pdf_gen";
 
 const Record = ({ auth }) => {
     const { hasAccess, getLayout } = useRole();
@@ -10,8 +9,8 @@ const Record = ({ auth }) => {
             <Head title="Record" />
             <Layout user={auth.user} header={<NavHeader headerName="Record" />}>
               {!hasAccess(auth.user.type, [2050, 2051, 2052]) ? <Unauthorized /> :
-                <div className="content" onClick={rnd_pdf_gen}>
-                  <button className="btn">Export Receipt and Dispatch Data</button>
+                <div className="content">
+                  
                 </div>
               }
             </Layout>
